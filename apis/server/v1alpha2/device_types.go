38eabad91445422f0d154ba957be2bc3197d0114
hasheddan <georgedanielmangum@gmail.com>
2020-07-15 16:40:11 -0500

    Add NetworkType to Device
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- apis/server/v1alpha2/device_types.go
+++ apis/server/v1alpha2/device_types.go
@@ -158,6 +158,9 @@ type DeviceParameters struct {
 	// +optional
 	ProjectSSHKeys []string `json:"projectSSHKeys,omitempty"`
 
+	// +optional
+	NetworkType *string `json:"networkType,omitempty"`
+
 	// Features can be used to require or prefer devices with optional features:
 	//
 	// features:
@@ -185,7 +188,6 @@ type DeviceObservation struct {
 	ProvisionPercentage resource.Quantity `json:"provisionPercentage,omitempty"`
 	IPv4                string            `json:"ipv4,omitempty"`
 	Locked              bool              `json:"locked"`
-	NetworkType         string            `json:"networkType,omitempty"`
 
 	// +optional
 	CreatedAt *metav1.Time `json:"createdAt,omitempty"`
