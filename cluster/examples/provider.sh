43ae6daac1893711955359e8de2f8e6c0fda8686
Marques Johansson <mjohansson@equinix.com>
2021-06-08 12:45:10 -0400

    add examples for vlans and attachments
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- cluster/examples/provider.sh
+++ cluster/examples/provider.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export BASE64ENCODED_PACKET_PROVIDER_CREDS=$(base64 credentials.txt | tr -d "\n")
-sed "s/BASE64ENCODED_PACKET_PROVIDER_CREDS/$BASE64ENCODED_PACKET_PROVIDER_CREDS/g" provider.yaml | kubectl create -f -
+export BASE64ENCODED_METAL_PROVIDER_CREDS=$(base64 credentials.txt | tr -d "\n")
+sed "s/BASE64ENCODED_METAL_PROVIDER_CREDS/$BASE64ENCODED_METAL_PROVIDER_CREDS/g" provider.yaml | kubectl create -f -
